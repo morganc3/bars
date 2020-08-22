30f4e419209c075b04de4a433b39cfe0c1d353f0
morganc3 <me@clbymrgn.com>
2020-08-21 20:06:07 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from lyrics import get_artist_id, get_random_song, get_lyrics
 from image import get_image, get_image_url
 from flask_api import status
-from flask import send_file
+from flask import send_file, Flask
 import tempfile
+import threading
+import os
+import discord 
 
+app = Flask(__name__)
 
-def generate_image(request):
+DISCORD_TOKEN=os.getenv("DISCORD_TOKEN")
+
+@app.route('/<artist>')
+def generate_image(artist):
     try:
-        artist = request.args.get('q')
         artist_id = get_artist_id(artist)
         song_url = get_random_song(artist_id)
         lyrics = get_lyrics(song_url)
@@ -18,10 +24,39 @@ def generate_image(request):
         file_name = tf.name + '.png'
         image.save(file_name)
         response = send_file(file_name, mimetype='image/png')
-        headers = {
-            "Cache-Control" : "no-cache, no-store, must-revalidate, public, max-age=0"
-        }
-        return (response, 200, headers)
+        response.cache_control.max_age = 0
+        return response
+
     except:
         return "error", status.HTTP_500_INTERNAL_SERVER_ERROR
 
+def create_server():
+      app.run(host='0.0.0.0', port=80)
+
+# start flask server
+threading.Thread(target=create_server).start()
+
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if '!bars' in message.content.lower():
+        try:
+            artist = message.content[6:]
+            artist_id = get_artist_id(artist)
+            song_url = get_random_song(artist_id)
+            lyrics = get_lyrics(song_url)
+
+            image_url = get_image_url(artist)
+            image = get_image(image_url, lyrics)
+            tf = tempfile.NamedTemporaryFile()
+            file_name = tf.name + '.png'
+            image.save(file_name)
+            await message.channel.send('🔥🔥🔥🔥')
+            await message.channel.send(file=discord.File(file_name))
+
+        except:
+            print('error')
+
+client.run(DISCORD_TOKEN)
